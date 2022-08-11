96dcb959d66a7ae4d27165374eb1477238f2cbe8
liangshenwen <40064388@qq.com>
2022-08-12 00:16:21 +0800

    add more

---
+++ learn_hadoop/python/market_price/README.md
@@ -0,0 +1,14 @@
+# 安装pyhdfs
+```
+python -m pip install pyhdfs
+```
+
+# pyhdfs文档
+https://pyhdfs.readthedocs.io/en/latest/index.html  
+
+# 安装mrjob
+```
+python -m pip install mrjob
+```
+# mrjob文档
+https://mrjob.readthedocs.io/en/latest/  
---
+++ learn_hadoop/python/mr_word_count.py
@@ -0,0 +1,20 @@
+import sys
+
+from mrjob.job import MRJob
+
+
+class MRWordFrequencyCount(MRJob):
+    def mapper(self, _, line):
+        yield "chars", len(line)
+        yield "words", len(line.split())
+        yield "lines", 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    sys.argv.append('-r')
+    sys.argv.append('hadoop')
+    sys.argv.append('my_file.txt')
+    MRWordFrequencyCount.run()
